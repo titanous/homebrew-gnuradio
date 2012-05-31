be9ad35c56ac1a76571805355e384a637ab5e382
Jonathan Rudenberg <jonathan@titanous.com>
2012-05-30 23:24:37 -0400

    Update gnuradio recipe for cmake

--- gnuradio.rb
+++ gnuradio.rb
@@ -1,44 +1,65 @@
 class Gnuradio < Formula
-    homepage 'http://gnuradio.org'
-    url  'http://gnuradio.org/redmine/attachments/download/326/gnuradio-3.6.0.tar.gz'
-    sha1 '7dfb82924cfff2e3af1dc744561d40a8030b05cc'
+  homepage 'http://gnuradio.org'
+  url  'http://gnuradio.org/redmine/attachments/download/326/gnuradio-3.6.0.tar.gz'
+  sha1 '7dfb82924cfff2e3af1dc744561d40a8030b05cc'
 
-    depends_on 'pkg-config' => :build
-    depends_on 'autoconf' => :build
-    depends_on 'automake' => :build
-    depends_on 'libtool' => :build
-    depends_on 'cmake' => :build
-    depends_on 'Cheetah' => :python
-    depends_on 'lxml' => :python
-    depends_on 'numpy' => :python
-    depends_on 'scipy' => :python
-    depends_on 'matplotlib' => :python
-    depends_on 'boost'
-    depends_on 'cppunit'
-    depends_on 'guile'
-    depends_on 'gsl'
-    depends_on 'fftw'
-    depends_on 'swig'
-    depends_on 'pygtk'
-    depends_on 'sdcc'
-    depends_on 'sdl'
-    depends_on 'pyqt'
-    depends_on 'pyqwt'
-    depends_on 'libusb'
+  depends_on 'cmake' => :build
+  depends_on 'Cheetah' => :python
+  depends_on 'lxml' => :python
+  depends_on 'numpy' => :python
+  depends_on 'scipy' => :python
+  depends_on 'matplotlib' => :python
+  depends_on 'boost'
+  depends_on 'cppunit'
+  depends_on 'guile'
+  depends_on 'gsl'
+  depends_on 'fftw'
+  depends_on 'swig'
+  depends_on 'pygtk'
+  depends_on 'sdcc'
+  depends_on 'sdl'
+  depends_on 'pyqt'
+  depends_on 'pyqwt'
+  depends_on 'libusb'
 
-    def install
-        # "manual" autoreconf to get '.dylib' extension on shared lib
-        system "aclocal --force -I config"
-        system "glibtoolize --copy --force"
-        system "autoconf --force"
-        system "autoheader --force"
-        system "automake --add-missing --copy --force-missing"
+  def patches
+    DATA
+  end
 
-        args = ["--disable-debug", "--disable-dependency-tracking",
-                "--prefix=#{prefix}",
-                "--mandir=#{man}"]
-
-        system "./configure", *args
-        system "make install"
+  def install
+    mkdir 'gnuradio-build' do
+      system 'cmake', '..',
+                      "-DCMAKE_PREFIX_PATH=#{prefix}",
+                      *std_cmake_args
+      system 'make'
+      system 'make install'
     end
+  end
 end
+
+__END__
+diff --git a/cmake/Modules/FindQwt.cmake b/cmake/Modules/FindQwt.cmake
+index f2cf344..0d3f45c 100644
+--- a/cmake/Modules/FindQwt.cmake
++++ b/cmake/Modules/FindQwt.cmake
+@@ -12,6 +12,7 @@ find_path (QWT_INCLUDE_DIRS
+ 	/usr/include/qwt
+ 	/opt/local/include/qwt
+ 	/sw/include/qwt
++	/usr/local/lib/qwt.framework/Headers
+ )
+ 
+ find_library (QWT_LIBRARIES
+diff --git a/grc/CMakeLists.txt b/grc/CMakeLists.txt
+index f54aa4f..db0ce3c 100644
+--- a/grc/CMakeLists.txt
++++ b/grc/CMakeLists.txt
+@@ -25,7 +25,7 @@ include(GrPython)
+ GR_PYTHON_CHECK_MODULE("python >= 2.5"     sys          "sys.version.split()[0] >= '2.5'"           PYTHON_MIN_VER_FOUND)
+ GR_PYTHON_CHECK_MODULE("Cheetah >= 2.0.0"  Cheetah      "Cheetah.Version >= '2.0.0'"                CHEETAH_FOUND)
+ GR_PYTHON_CHECK_MODULE("lxml >= 1.3.6"     lxml.etree   "lxml.etree.LXML_VERSION >= (1, 3, 6, 0)"   LXML_FOUND)
+-GR_PYTHON_CHECK_MODULE("pygtk >= 2.10.0"   gtk          "gtk.pygtk_version >= (2, 10, 0)"           PYGTK_FOUND)
++GR_PYTHON_CHECK_MODULE("pygtk >= 2.10.0"   pygtk        True                                        PYGTK_FOUND)
+ GR_PYTHON_CHECK_MODULE("numpy"             numpy        True                                        NUMPY_FOUND)
+ 
+ ########################################################################
