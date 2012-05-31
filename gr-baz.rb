bf199832fadb30a9dabdedfc8e3cfe5ecae5da3f
Jonathan Rudenberg <jonathan@titanous.com>
2012-05-31 00:48:23 -0400

    Add gr-baz build deps

--- gr-baz.rb
+++ gr-baz.rb
@@ -4,10 +4,14 @@ class GrBaz < Formula
   homepage 'http://wiki.spench.net/wiki/Gr-baz'
   head 'git://github.com/balint256/gr-baz.git'
 
+  depends_on 'pkg-config' => :build
+  depends_on 'automake' => :build
+  depends_on 'libtool' => :build
   depends_on 'libusb'
 
   def install
     args = ["--prefix=#{prefix}"]
+    system "autoreconf -i"
     system "./configure", *args
     system "make install"
   end
