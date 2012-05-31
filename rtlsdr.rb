81beab8c9cad42d7f5cf0d61858a9fe03b3d6474
Jonathan Rudenberg <jonathan@titanous.com>
2012-05-31 10:09:35 -0400

    Add support for keenerd's rtl_fm fork of rtlsdr

--- rtlsdr.rb
+++ rtlsdr.rb
@@ -2,13 +2,25 @@ require 'formula'
 
 class Rtlsdr < Formula
   homepage 'sdr.osmocom.org/trac/wiki/rtl-sdr'
-  head 'git://git.osmocom.org/rtl-sdr.git'
+
+  if ARGV.include?('--fm-fork')
+    head 'git://github.com/keenerd/rtl-sdr.git'
+    depends_on 'sox'
+  else
+    head 'git://git.osmocom.org/rtl-sdr.git'
+  end
 
   depends_on 'pkg-config' => :build
   depends_on 'automake' => :build
   depends_on 'libtool' => :build
   depends_on 'cmake' => :build
   depends_on 'libusb'
+
+  def options
+    [
+      ['--fm-fork', 'Install the fork from keenerd with rtl_fm']
+    ]
+  end
 
   def install
     args = ["--prefix=#{prefix}"]
