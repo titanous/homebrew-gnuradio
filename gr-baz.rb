2f056fbc197a483483146e47d71b321fd0c455a0
Jonathan Rudenberg <jonathan@titanous.com>
2012-06-06 12:46:59 -0400

    Force gr-baz to a working revision. Fixes #1.

--- gr-baz.rb
+++ gr-baz.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class GrBaz < Formula
   homepage 'http://wiki.spench.net/wiki/Gr-baz'
-  head 'https://github.com/balint256/gr-baz.git'
+  head 'https://github.com/balint256/gr-baz.git', :revision => '63c68feb'
 
   depends_on 'pkg-config' => :build
   depends_on 'automake' => :build
