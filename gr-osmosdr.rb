c01ea7dbb0f6a26504c285ebec4528090de3a7c9
Jonathan Rudenberg <jonathan@titanous.com>
2012-08-18 16:12:20 -0400

    Link gr-osmosdr against Homebrew python. Closes #8.

--- gr-osmosdr.rb
+++ gr-osmosdr.rb
@@ -10,9 +10,15 @@ class GrOsmosdr < Formula
 
   def install
     mkdir 'build' do
-      system 'cmake', '..', *std_cmake_args
+      system 'cmake', '..', *std_cmake_args << "-DPYTHON_LIBRARY=#{python_path}/Frameworks/Python.framework/"
       system 'make'
       system 'make install'
     end
+  end
+
+  def python_path
+    python = Formula.factory('python')
+    kegs = python.rack.children.reject { |p| p.basename.to_s == '.DS_Store' }
+    kegs.find { |p| Keg.new(p).linked? } || kegs.last
   end
 end
